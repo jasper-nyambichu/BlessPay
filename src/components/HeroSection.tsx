164ce215eb65911d986a620158fb9e23dab62b3d
Moseti Dickson <nyambichujasper@gmail.com>
2025-12-17 19:34:52 +0300

    path corrected

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button } from "@/components/ui/button";
+import { Button } from "./ui/button";
 import { ArrowRight, Church } from "lucide-react";
 import Link from "next/link";
 
